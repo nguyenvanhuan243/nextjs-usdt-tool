c47ec2f91120c5728cd16a26fc2e2c6f982338d1
Huan Nguyen <nguyenvanhuan243@gmail.com>
2025-03-08 17:56:09 +0700

    Update wallet api service

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://demo-wallet-exchange.fiotech.org",
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://demo-wallet-bscexchange-com.onrender.com",
   },
 };
 
